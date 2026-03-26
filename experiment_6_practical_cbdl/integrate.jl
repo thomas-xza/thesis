593faf7d65851cbf47479374c2f5ac542d050f7a
qdosgit4 <244482937+qdosgit4@users.noreply.github.com>
2026-03-26 15:52:43 +0000

    Add files via upload

--- experiment_6_practical_cbdl/integrate.jl
+++ experiment_6_practical_cbdl/integrate.jl
@@ -1,42 +1,53 @@
 using Plots
+using Trapz
 
 
-files = filter(f -> endswith(f, ".txt"), readdir())
-
-
-x_interval = 1.57:0.015625:4.71
-
-x_tuple = Tuple(x_interval)
-
-
-
-for file in files
-
-    name_only = replace(file, ".txt" => "")
-
-    raw_content = read(file, String)
+function integrate_pdf(xs :: NTuple{N, Float64} , ys :: NTuple{N, Float64}) where N
     
-    y_vals = split(rstrip(raw_content), "\n")
+        # p = plot(collect(x_tuple), collect(y_floats))
 
-    # println(y_vals)
+        # savefig(p, replace(file, ".txt" => ".pdf"))
 
-    y_floats = Base.front(Tuple(parse.(Float64, y_vals)))
+        # println(y_floats)
 
-    println(typeof(x_tuple))
+        # println(zipped_x_y)
 
-    println(typeof(y_floats))
+        return trapz(collect(xs), collect(ys))    
 
-    # println(y_floats)
-
-    zipped_x_y = Tuple(zip(x_tuple, y_floats))
-
-    # println(zipped_x_y)
-
-    p = plot(collect(x_tuple), collect(y_floats))
-        
-    savefig(p, replace(file, ".txt" => ".pdf"))
-    
 end
 
 
-println(x_tuple)
+function integrate_pdfs_set(a :: Float64 = 1.57, b :: Float64 = 4.71)
+
+    files = filter(f -> endswith(f, ".txt"), readdir())
+
+    x_interval = 1.57:0.015625:4.71
+
+    integrands = Float64[]
+
+    for file in files
+
+        name_only = replace(file, ".txt" => "")
+
+        raw_content = read(file, String)
+
+        y_vals = split(rstrip(raw_content), "\n")
+
+        y_floats = Base.front(Tuple(parse.(Float64, y_vals)))
+
+        # println(typeof(x_tuple))
+
+        # println(y_vals)
+
+        # println(typeof(y_floats))
+
+        integrands = vcat(integrands, integrate_pdf(Tuple(x_interval), y_floats))
+
+    end
+
+    return integrands
+
+end
+
+
+println(integrate_pdfs_set())
