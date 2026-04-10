65e1331785b7e0f2d19acd411c8cac62b9fd54ae
qdosgit5 <qdosgit4+5@gmail.com>
2026-04-10 19:34:44 +0000

    Shannon entropy done

--- past_experiments/experiment_1_dirichlet_cbdl/alpha_test.jl
+++ past_experiments/experiment_1_dirichlet_cbdl/alpha_test.jl
@@ -1,6 +1,8 @@
-# using Debugger
 
-using Plots
+# using Plots, SpecialFunctions
+
+using SpecialFunctions
+
 
 function gen_predictive_dist(d ::Int64, m ::Int64, n ::Int64, y ::Int64)
 
@@ -14,9 +16,15 @@ function gen_predictive_dist(d ::Int64, m ::Int64, n ::Int64, y ::Int64)
 
     res = zeros(Float32, permutations_n)
 
+    all_perms = Vector{UInt64}[]
+
     for i = UInt128(0):UInt128(permutations_n - 1)
 
         alpha = gen_permutation(i, m, n)
+
+        all_perms = [all_perms..., alpha]
+
+        # println(alpha)
 
         for j = 1:length(alpha)
 
@@ -36,7 +44,7 @@ function gen_predictive_dist(d ::Int64, m ::Int64, n ::Int64, y ::Int64)
 
     end
 
-    return x_range, res
+    return x_range, res, all_perms
 
 end
 
@@ -64,18 +72,85 @@ function gen_permutation(i ::UInt128, m ::Int64, n ::Int64)
 end
 
 
+function calc_entropy(all_perms :: Vector{Vector{UInt64}})
+
+    # println(x_range)
+
+    println(all_perms)
+
+    all_entropy = Vector{Float64}[]
+
+    highest_entropy = -1024
+
+    lowest_entropy = 1024
+
+    for (i, perm) in enumerate(all_perms)
+
+        perm = perm .+ 1
+
+        sum_a = sum(perm)
+
+        b_a = 1
+
+        for a in perm
+
+            b_a *= factorial(a - 1)
+
+        end
+
+        b_a = b_a / factorial(big(sum_a - 1))
+
+        summation = 0
+
+        for a in perm
+
+            summation += (a - 1) * digamma(a)
+
+        end
+        
+        all_entropy = [
+            all_entropy...,
+            log(b_a) + (sum_a - length(perm)) * digamma(sum_a) - summation
+        ]
+
+        if all_entropy[i] > highest_entropy
+
+            highest_entropy = all_entropy[i]
+
+            println("new highest ", highest_entropy, perm)
+
+        end
+
+        if  all_entropy[i] < lowest_entropy
+
+            lowest_entropy = all_entropy[i]
+
+            println("new lowest ", lowest_entropy, perm)
+            
+        end
+
+    end
+
+end
+
+
 # println(gen_permutation(UInt128(0x1), 8, 8))
 
 # println(gen_permutation(UInt128(0x1_0000_0000_0000), 8, 16))
 
 #res = gen_predictive_dist(6, 5, 20, 1)
 
-@time x_range, res = gen_predictive_dist(1, 3, 3, 1)
+x_range, res, all_perms = gen_predictive_dist(1, 3, 3, 1)
 
-println(length(res))
+calc_entropy(all_perms)
+
+# @time x_range, res = gen_predictive_dist(1, 3, 3, 1)
+
+# println(length(res))
 
 # println(res)
 
-p = bar(x_range, res, title="Dirichlet plot")
+# p = bar(x_range, res, title="Dirichlet plot")
 
-savefig(p, "bar.pdf")
+# savefig(p, "bar.pdf")
+
